681e702d656977c452fa493b1fec8c0ec7f0b365
randomsamples <randomsamples@github.com>
2020-08-22 23:49:42 -0700

    Fix sample python test lint errors

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,30 @@
-import pytest
+"""Just a simple pytest.
+
+A sample test to get things started and to demonstrate proper pydoc strings.
+Use google recommended style: https://google.github.io/styleguide/pyguide.html
+"""
+
+
+def return_it(obj: object):
+    """Returns the argument passed in.
+
+    Does nothing but simply return whatever is passed in.
+
+    # Parameters:
+    #    obj (object):Any object you want to pass.
+
+    # Returns:
+    #    Simply return the object that was passed in.
+    """
+
+    return obj
 
 
 def test_foo():
-    assert(True)
+    """A simple test that always passes.
+
+    Just to make sure pytest is passing.
+    """
+
+    test_object = {}
+    assert return_it(test_object) == test_object
